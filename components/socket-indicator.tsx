945824a7cc4ef77d10102853b6b493a09b07dc1b
Diego S <dieg.slva@gmail.com>
2023-12-04 10:40:15 -0300

    dd

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -12,7 +12,7 @@ export const SocketIndicator = () => {
         variant="outline" 
         className="bg-yellow-600 text-white border-none"
       >
-        Fallback: Polling every 1s
+        Fallback: negociando a cada 1s
       </Badge>
     )
   }
@@ -22,7 +22,7 @@ export const SocketIndicator = () => {
       variant="outline" 
       className="bg-emerald-600 text-white border-none"
     >
-      Live: Real-time updates
+      Ao vivo: Em tempo real
     </Badge>
   )
 }
