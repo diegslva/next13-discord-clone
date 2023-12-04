2699981df250c0121a843075ba49868606e970c8
Diego S <dieg.slva@gmail.com>
2023-12-04 17:30:05 -0300

    dd

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -18,6 +18,11 @@ import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
 import { EmojiPicker } from "@/components/emoji-picker";
 
+
+
+
+
+
 interface ChatInputProps {
   apiUrl: string;
   query: Record<string, any>;
--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -5,12 +5,16 @@ import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
 import { useTheme } from "next-themes";
 import i18n from '@emoji-mart/data/i18n/pt.json'
+import Picker2, { Categories} from 'emoji-picker-react';
 
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { ca } from "date-fns/locale";
+
+
 
 interface EmojiPickerProps {
   onChange: (value: string) => void;
@@ -33,13 +37,27 @@ export const EmojiPicker = ({
         sideOffset={40}
         className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
       >
+       {/*
         <Picker
           i18n={i18n}
           theme={resolvedTheme}
           data={data}
           onEmojiSelect={(emoji: any) => onChange(emoji.native)}
-        />
-      </PopoverContent>
+          />
+       */}
+        <Picker2 
+          previewConfig={
+            {
+              defaultCaption: "Como você esta!?", // defaults to: "What's your mood?"
+            }
+          }
+        
+        
+        
+          searchPlaceholder="Pesquisar"
+          onEmojiClick={(emojiData, event) => onChange(emojiData.emoji)} />
+     
+        </PopoverContent>
     </Popover>
   )
 }
